962e66be0ee67d8aa8ca1738210ead4baeec7c5b
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-26 22:08:20 +0530

    Checking,If The Permission Of Camera is Granted or not.

--- app/src/main/java/com/example/textfetchorapp/MainActivity.java
+++ app/src/main/java/com/example/textfetchorapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.textfetchorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
         imageView=findViewById(R.id.imageId);
         //Finding The TextById
         textView=findViewById(R.id.textAfterExtract);
+        if(checkSelfPermission(Manifest.permission.CAMERA)!=
+                PackageManager.PERMISSION_GRANTED) {
+
+        }
     }
 
     public void doProcess(View view) {
