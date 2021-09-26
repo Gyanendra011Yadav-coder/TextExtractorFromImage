f8ab5a7a048d1bfece790eb5b500ca5771c6fceb
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-27 00:08:34 +0530

    added the permission in the Manifest.

--- app/src/main/java/com/example/textfetchorapp/MainActivity.java
+++ app/src/main/java/com/example/textfetchorapp/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,9 +24,12 @@ public class MainActivity extends AppCompatActivity {
         imageView=findViewById(R.id.imageId);
         //Finding The TextById
         textView=findViewById(R.id.textAfterExtract);
+
+        //Checking, if the Permission of Camera is Granted or not
         if(checkSelfPermission(Manifest.permission.CAMERA)!=
                 PackageManager.PERMISSION_GRANTED) {
-
+            //Showig the Error to the user and Making Providing the Acces to The Camera.
+                requestPermissions(new String[]{Manifest.permission.CAMERA},404);
         }
     }
 
