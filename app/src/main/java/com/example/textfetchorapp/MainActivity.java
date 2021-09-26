b1b84ee5dc82ab04265e5a59cdccea22b86ef338
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-09-26 18:48:07 +0530

    Fetching the Image and text and storing it to the defined Variables.

--- app/src/main/java/com/example/textfetchorapp/MainActivity.java
+++ app/src/main/java/com/example/textfetchorapp/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.textfetchorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    //Declaring the Variables
     ImageView imageView;
-    TextView TextView;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Finding Image By Id
+        imageView=findViewById(R.id.imageId);
+        //Finding The TextById
+        textView=findViewById(R.id.textAfterExtract);
     }
 
     public void doProcess(View view) {
